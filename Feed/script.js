714cfad0e72a9bca28cf75d91042c1109ba9c994
Nicolas <nicolashenrisilva@outlook.com>
2023-06-28 20:39:51 -0300

    botao ver mais e menu hamburguer por fazer

--- Feed/script.js
+++ Feed/script.js
@@ -13,3 +13,19 @@ function mostrarMenu (){
 
     }
 }
+
+// function leiaMais(){
+//     var pontos=document.getElementById("pontos");
+//     var maisTexto=documento.getElementById("mais");
+//     var btnLeiaMais=document.getElementById("btnLeiaMais");
+
+//     if(pontos.style.display === "none"){
+//         pontos.style.display="inline";
+//         maisTexto.style.display="none";
+//         btnLeiaMais.innerHTML="Leia Mais";
+//     }else{
+//         pontos.style.display="none";
+//         maisTexto.style.display="inline";
+//         btnLeiaMais.innerHTML="Leia Menos";
+//     }
+// }
